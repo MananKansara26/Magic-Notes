f5250147c3976499ea0a0c60145e023ce288a443
Manan Kansara <mananpk2002@gmail.com>
2024-11-04 21:41:37 +0530

    Code refactored

--- app.js
+++ app.js
@@ -1,98 +1,60 @@
-console.log("Welcome to javascript file");
-showNotes();
+console.log("Magic Notes App Loaded");
+document.addEventListener("DOMContentLoaded", showNotes);
 
-//if user adds a note add to the local storage
-let addBtn=document.getElementById('addBtn');
-addBtn.addEventListener('click',function(e)
-{
-    let addTxt=document.getElementById('addTxt');
-    let notes=localStorage.getItem('notes');
-    if(notes == null)
-    {
-        notesObj=[];//if there is no item the we have to set object as null otherwise push operation can not be done to the object that is not exist
-    }
-    else
-    {
-        notesObj=JSON.parse(notes);
-    }
-    notesObj.push(addTxt.value);
-    localStorage.setItem('notes',JSON.stringify(notesObj));//coz we have to store as a string. Every time we add notes, notes(key in local storage) is overwrite in local storage while in notesObj one entry is added
-    addTxt.value="";//remove written text after adding the note
-    showNotes();
+// Function to add a note
+function addNote() {
+  const addTxt = document.getElementById("addTxt");
+  let notes = getNotesFromLocalStorage();
+
+  notes.push(addTxt.value);
+  localStorage.setItem("notes", JSON.stringify(notes));
+
+  addTxt.value = ""; // Clear textarea after adding note
+  showNotes();
 }
-);  
 
-//function to show notes from the local storage
-function showNotes()
-{
-    let notes=localStorage.getItem('notes');
-    if(notes == null)
-    {
-        notesObj=[];//if there is no item the we have to set object as null otherwise push operation can not be done to the object that is not exist
-    }
-    else
-    {
-        notesObj=JSON.parse(notes);
-    }
-    let html="";
-    notesObj.forEach(function(element,index)//index is a optional parameter and used 
-    {
-        html+=`<div class="noteCard my-2 mx-2" style="width: 18rem;">
-        <div class="card-body">
-          <h5 class="card-title">Note ${index+1}</h5>
-          <p class="card-text">${element}</p>
-          <button id="index" onclick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button><!--by writing this.id the id is passed to the function of this button-->
-        </div>
+// Function to retrieve notes from local storage
+function getNotesFromLocalStorage() {
+  let notes = localStorage.getItem("notes");
+  return notes ? JSON.parse(notes) : [];
+}
+
+// Function to show notes from local storage
+function showNotes() {
+  const notes = getNotesFromLocalStorage();
+  const notesContainer = document.getElementById("notes");
+  notesContainer.innerHTML = notes.length
+    ? notes.map((note, index) => createNoteHTML(note, index)).join("")
+    : `Nothing to show. Add notes, please!`;
+}
+
+// Function to generate HTML for each note
+function createNoteHTML(noteText, index) {
+  return `
+    <div class="noteCard my-2 mx-2" style="width: 18rem;">
+      <div class="card-body">
+        <h5 class="card-title">Note ${index + 1}</h5>
+        <p class="card-text">${noteText}</p>
+        <button onclick="deleteNote(${index})" class="btn btn-primary">Delete Note</button>
+      </div>
     </div>`;
-    }
-    );
-    let notesElm=document.getElementById('notes');//this note is id of div of all cards
-    if(notesObj.length!=0)
-    {
-        notesElm.innerHTML=html;
-    }
-    else
-    {
-        notesElm.innerHTML=`Nothing to show. Add notes please!`;
-    }
 }
 
-//function to delete function
-function deleteNote(index)
-{
-    let notes=localStorage.getItem('notes');
-    if(notes == null)
-    {
-        notesObj=[];//if there is no item the we have to set object as null otherwise push operation can not be done to the object that is not exist
-    }
-    else
-    {
-        notesObj=JSON.parse(notes);
-    }
-    notesObj.splice(index,1);
-    localStorage.setItem('notes',JSON.stringify(notesObj));
-    showNotes();
+// Function to delete a note
+function deleteNote(index) {
+  let notes = getNotesFromLocalStorage();
+  notes.splice(index, 1);
+  localStorage.setItem("notes", JSON.stringify(notes));
+  showNotes();
 }
 
-let search=document.getElementById('searchTxt');
-search.addEventListener('input',function()
-{
-    let inputVal=search.value.toLowerCase();//lowercase coz there will not case diffrent in search
-    // console.log(inputVal);
-    let noteCards=document.getElementsByClassName('noteCard');
-    // console.log(noteCards);
-    Array.from(noteCards).forEach(function(element)
-    {
-        let cardTxt=element.getElementsByTagName("p")[0].innerText.toLowerCase();//first p element of card which is decription of note. here also use lowercase so that both are in lower case so that searchin will efficient
-        if(cardTxt.includes(inputVal))
-        {
-            element.style.display="block";
-        }
-        else
-        {
-            element.style.display="none";
-        }
-    }
-    )
+// Function to search notes based on input
+function searchNotes() {
+  const searchTxt = document.getElementById("searchTxt").value.toLowerCase();
+  const noteCards = document.getElementsByClassName("noteCard");
+
+  Array.from(noteCards).forEach((card) => {
+    const cardText = card.querySelector(".card-text").innerText.toLowerCase();
+    card.style.display = cardText.includes(searchTxt) ? "block" : "none";
+  });
 }
-)
